b2faae072dfc10abf0974a7e1082d86d0650dee7
roushan0612 <roushan.dec@gmail.com>
2024-09-17 18:11:51 +0530

    Add files via upload

---
+++ Day1/index.py
@@ -0,0 +1,32 @@
+# Comments:
+# # for single-line comments and triple quotes ''' or """ for multi-line comments.
+"""
+multi-line comment
+as it is ignored by python interprator
+"""
+# text datatypes
+a="double quote"
+b='s'
+c='''yytyuthjf''' #multiple line
+print(type(b))
+
+#numerics data type
+# int
+intdata=5
+#float
+floatdata=5.8
+#complex
+complexdata=1+1j
+print(type(intdata))
+print(type(floatdata))
+print(type(complexdata))
+
+#boolean
+bolla=True
+bollb=False
+print(type(bolla))
+print(type(bollb))
+d=int(b)
+print(type(d))
+print(type(b))
+print(d)
