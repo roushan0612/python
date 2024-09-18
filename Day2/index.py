525f42019f7d2b0d1e875e7b2b6f85561926f62c
roushan0612 <roushan.dec@gmail.com>
2024-09-18 18:27:28 +0530

    Add files via upload

---
+++ Day2/index.py
@@ -0,0 +1,28 @@
+a=5
+b=6
+print(a*b)
+print(a/b)
+print(a-b)
+print(a**b)
+print(a//b)
+
+print("\n")  # new line
+print("\t") #one tab space 4 space
+print("\b")  #white space
+print("\'")   #single quote
+print("\\")    #backslash
+print("\033[92m {}\033[00m" .format("hello"))  #a string template where we can insert var value and print color
+a=[3,5]
+'''
+Fibonacci sequence,
+first +second num =third 
+a first num 
+b second 
+c third
+check=false
+
+'''
+a=1
+b=1
+c=2
+check=a+b==c  # it will return a bool -------if false means not Fibonacci sequence,
